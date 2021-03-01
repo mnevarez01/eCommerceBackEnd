00da5c5111b8dce4280391c95936842fa8ea94fc
Maria Nevarez <mnevarez1220@gmail.com>
2021-03-01 12:49:03 -0700

    updated category and product routes

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -1,28 +1,51 @@
-//!! all API routes for the categories
-
-//TODO: the GET, POST=> will need to create DB information 
-//TODO: PUT route, and delete route. 
+//all API routes for the categories: 'api/categories'
 
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 router.get('/', (res, req) => {
-
+  Category.findAll({
+    include: [Product]
+  })
+    .then((categories) => res.status(200).json(categories))
+    .catch(err => res.status(500).json(err))
 })
 
 router.get('/:id', (res, req) => {
-
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+    .then((categories) => res.status(200).json(categories))
+    .catch(err => res.status(400).json(err))
 })
 
 router.post('/', (req, res) => {
+  Category.create(req.body)
+    .then(category => res.status(200).json(category))
+    .catch(err => res.status(400).json(err))
 
 })
 
 router.put('/:id', (req, res) => {
-
+  Category.update({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(category => res.status(200).json(category))
+    .catch(err => res.status(400).json(err))
 })
 router.delete('/:id', (req, res) => {
-
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((category) => res.status.apply(200).json(category))
+    .catch(err => res.status(400).json(err))
 })
 
 module.exports = router
--- routes/api/productRoutes.js
+++ routes/api/productRoutes.js
@@ -1,4 +1,4 @@
-//!! all API routes for the product
+//API routes for Products: '/api/products'
 
 //TODO: the GET, POST=> will need to create DB information 
 //TODO: PUT route, and delete route. 
@@ -7,21 +7,64 @@ const router = require('express').Router();
 const { Category, Product, Tag, ProductTag } = require('../../models');
 
 router.get('/', (res, req) => {
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+    .then(products => res.status(200).json(products))
+    .catch(err => res.status(500).json(err))
 
 })
 
 router.get('/:id', (res, req) => {
-
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+    .then(product => res.statusCode(200).json(product))
+    .catch(err => res.status(500).json(err))
 })
 
 router.post('/', (req, res) => {
-
+  Product.create(req.body)
+    .then(product => {
+      if (req.body.tagsIds.length) {
+        const productTagidArr = rex.body.tagsIds.map((tagsIds) => {
+          return {
+            product_id: product.id,
+            tag_id
+          }
+        })
+        return ProductTag.bulkCreate(productTagidArr)
+      }
+    })
+    .catch(err => res.status(500).json(err))
 })
 
 router.put('/:id', (req, res) => {
 
 })
 router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(product => res.status(200).json(product))
+    .catch(err => res.status(500).json(err))
 
 })
 
