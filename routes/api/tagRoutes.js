8077ec29ccc22126753ddcf3b00af02ef2919f6d
Maria Nevarez <mnevarez1220@gmail.com>
2021-03-01 13:28:36 -0700

    Updated tag routes

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -8,21 +8,61 @@ const router = require('express').Router();
 const { Product, Tag, ProductTag } = require('../../models');
 
 router.get('/', (res, req) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+    .then(tags => res.status(200).json(tags))
+    .catch(err => res.status(500).json(err))
 
 })
 
 router.get('/:id', (res, req) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
 
+  })
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(400).json(err))
 })
 
 router.post('/', (req, res) => {
+  Tag.create(req.body)
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(400).json(err))
 
 })
 
 router.put('/:id', (req, res) => {
-
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(400).json(err))
 })
+
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(400).json(err))
 
 })
 
